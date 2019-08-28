f14b6debc29b6283e7856a6f29995ce147518cb5
Jiří Noha <jiri.hegar@centrum.cz>
2019-08-28 17:49:14 +0200

    jedu

---
+++ ZaloZneNewATest/ZaloZneNewATest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ZaloZneNewATest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //smazat a znovu nahrata
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
